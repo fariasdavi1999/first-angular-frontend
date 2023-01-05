c0256fbdc50a081e5e1601c26b09b0b4af253d0c
davinascimentoConnectDF <davi.nascimento@connectdf.com.br>
2023-01-05 11:39:53 -0300

    Testando abrir notif ao clicar

--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -5,46 +5,34 @@ import { Observable } from 'rxjs';
 import { Cliente } from './cliente';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  private API = 'http://localhost:8080/api/clientes';
 
-  private API = 'http://localhost:8080/api/clientes'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   listarTodos() {
-
     return this.http.get<Array<Cliente>>(`${this.API}/listar-clientes`);
-
   }
 
   getByCpf(cpfCliente: string): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.API}/cpf/${cpfCliente}`)
+    return this.http.get<Cliente>(`${this.API}/cpf/${cpfCliente}`);
   }
 
   getById(id: any): Observable<Cliente> {
-
-    return this.http.get<Cliente>(`${this.API}/${id}`)
-
+    return this.http.get<Cliente>(`${this.API}/${id}`);
   }
 
   getIncluir(request: Cliente) {
-
     return this.http.post<Cliente>(this.API, request);
-
   }
 
   getAlterar(id: any, request: Cliente) {
-
     return this.http.put<Cliente>(`${this.API}/${id}`, request);
-
   }
 
   getExcluir(id: any) {
-
     return this.http.delete<Cliente>(`${this.API}/${id}`);
-
   }
-
 }
